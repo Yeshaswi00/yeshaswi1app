20177b4e3b1bbc3f7db246d5491eccfd87d2868d
Yeshaswi00 <yeshaswimusali987@gmail.com>
2023-09-01 17:45:38 +0530

    ppr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import Products from './products';
+import Project from './projects';
+import Reviews from './review';
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    <Products></Products>
+   <Project></Project>
+   <Reviews></Reviews>
+     
+      
     </div>
   );
 }
