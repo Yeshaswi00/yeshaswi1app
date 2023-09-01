37755b0adb215a425e587bd485dcc1d81783b82a
Yeshaswi00 <yeshaswimusali987@gmail.com>
2023-09-01 19:50:09 +0530

    acc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
-import Sorting from './Sorting';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Accordion from './Accordion';
 
 
 function App() {
   return (
     <div>
-      <Sorting></Sorting> 
+      <Accordion></Accordion> 
     </div>
   );
 }
