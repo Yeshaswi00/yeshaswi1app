88c4713d42fa37237e3e616590931d30d98b2a27
Yeshaswi00 <yeshaswimusali987@gmail.com>
2023-09-07 18:37:10 +0530

    sun and earth

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,13 @@ import {Link,Outlet} from 'react-router-dom';
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Container from './Container';
 
 
 function App() {
 
   return (
-    <div className='mybox'>
-      <h1>welcome page</h1>
-      <Link to="/aboutus">ABoutUs</Link>
-      &nbsp;&nbsp;
-      <Link to="/courses">Courses</Link>
-     <Outlet></Outlet>
-    </div>
+   <Container></Container>
     
   );
 }
---
+++ src/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <div className="container">
+        <div className='sun'></div>
+        <div className='earth'>
+            <div className='moon'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Container
